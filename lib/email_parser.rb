3643ae9ab3f64524e1920d0206b64a295bdfa265
zhifeng ou <zhifengou@hotmail.com>
2018-09-23 22:24:01 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,9 +3,9 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser 
-  attr_accessor:email 
-  def initialize(email)
-    @email=email
+  attr_accessor:emails 
+  def initialize(emails)
+    @emails=emails
   end
   
   def parse 
